2d9cc9f935131ef185b4a91a0a2abc6f56789b40
Rich Bielby <richard.bielby@education.gov.uk>
2026-06-24 18:06:40 +0100

    Update r-renv-packages.R

--- r-renv-packages.R
+++ r-renv-packages.R
@@ -1,15 +1,18 @@
 # Package set up
 renv::init()
-renv::snapshot(
-   c(
-      "dfe-analytical-services/dfeshiny", 
-      "dfe-analytical-services/dfeR", 
-      "dfe-analytical-services/eesyapi", 
-      "dfe-analytical-services/eesyscreener", 
-      "dfe-analytical-services/shinyGovstyle",
-      "shiny",
-      "terra",
-      "sf"
-   )
+renv::record(
+  c(
+    "renv",
+    "dfe-analytical-services/dfeshiny",
+    "dfe-analytical-services/dfeR",
+    "dfe-analytical-services/eesyapi",
+    "dfe-analytical-services/eesyscreener",
+    "dfe-analytical-services/shinyGovstyle",
+    "shiny",
+    "shinytest2",
+    "terra",
+    "sf"
+  )
 )
 renv::restore()
+renv::snapshot()
